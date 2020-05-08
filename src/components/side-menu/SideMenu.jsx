149306fc66ebc1f6d6a5cebab318d680c3f5501c
Saleh Alaswad <alaswad@Salehs-Air.fritz.box>
2020-05-08 23:48:29 +0200

    deploy my app

--- src/components/side-menu/SideMenu.jsx
+++ src/components/side-menu/SideMenu.jsx
@@ -1,6 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+const list = {
+    newArrivals: [
+        'Clothes',
+        'Shoes & Accessoires',
+        'Jackets'
+    ],
+    product: [
+        'Hoodies & Sweatshirts',
+        'Cardigans & Jumpers',
+        'Jackets & Coats',
+        'Shirts',
+        'Blazers & Suits',
+        'T-Shirts & Tanks',
+        'Trousers',
+        'Jeans',
+        'Jackets & Coats',
+        'Shirts',
+        'Accessories',
+        'Shoes',
+        'Socks',
+        'Nightwear',
+        'Underwear',
+        'Sportwear',
+        'Shorts',
+        'Swimwear',
+    ],
+    sale: [
+        'T-Shirts',
+        'Shoes',
+        'Trousers'
+    ]
+}
+
 const SideMenu = ({ match, className }) => {
     return (
         <div className={className}>
@@ -9,82 +42,19 @@ const SideMenu = ({ match, className }) => {
                     <li >
                         <strong >New Arrivals</strong>
                         <ul >
-                            <li >
-                                <Link to={match.url} >Clothes</Link>
-                            </li>
-                            <li >
-                                <Link to={`${match.url}?product=shoes`}> Shoes &amp; Accessories</Link>
-                            </li>
-                            <li >
-                                <Link to={`${match.url}?product=jeans`}> Jackets</Link>
-                            </li>
+                            {list.newArrivals.map(product => <li ><Link to={`${match.url}/${product}`} >{product}</Link></li>)}
                         </ul>
                     </li>
                     <li >
                         <strong >Product</strong>
                         <ul >
-                            <li >
-                                <Link to={match.url} >Hoodies &amp; Sweatshirts</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Cardigans &amp; Jumpers</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Jackets &amp; Coats</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Shirts</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Blazers &amp; Suits</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> T-Shirts &amp; Tanks</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Trousers</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Jeans</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Accessories</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Shoes</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Socks</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Nightwear</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Underwear</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Sportwear</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Shorts</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Swimwear</Link>
-                            </li>
+                            {list.product.map(product => <li ><Link to={`${match.url}/${product}`} >{product}</Link></li>)}
                         </ul>
                     </li>
                     <li >
                         <strong >Sale</strong>
                         <ul >
-                            <li >
-                                <Link to={match.url} >T-Shirts</Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Shoes </Link>
-                            </li>
-                            <li >
-                                <Link to={match.url}> Trousers</Link>
-                            </li>
+                            {list.sale.map(product => <li ><Link to={`${match.url}/${product}`} >{product}</Link></li>)}
                         </ul>
                     </li>
                 </ul>
